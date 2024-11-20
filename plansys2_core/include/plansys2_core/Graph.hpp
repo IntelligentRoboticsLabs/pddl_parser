8a981d7f2654be57b78db3c98184a97a8b9f37cd
Gustavo <g.rezendesilva@tudelft.nl>
2024-11-20 20:12:20 +0100

    add ActionVariant and backtraverse to Graph

--- plansys2_core/include/plansys2_core/Graph.hpp
+++ plansys2_core/include/plansys2_core/Graph.hpp
@@ -16,18 +16,20 @@
 #define PLANSYS2_CORE__GRAPH_HPP_
 
 #include <iostream>
+#include <deque>
 
 #include "plansys2_core/Types.hpp"
 #include "plansys2_core/Action.hpp"
 #include "plansys2_msgs/msg/derived.hpp"
 #include "plansys2_msgs/msg/node.hpp"
+#include "plansys2_msgs/msg/tree.hpp"
 
 namespace plansys2
 {
 
 class NodeVariant {
 public:
-  using NodeType = std::variant<plansys2::Predicate, plansys2::Function, plansys2::Derived, plansys2::Action, plansys2::DurativeAction>;
+  using NodeType = std::variant<plansys2::Predicate, plansys2::Function, plansys2::Derived, plansys2::ActionVariant>;
 
   template <typename NodeT>
   NodeVariant(NodeT node) : node_(std::make_shared<NodeType>(node)) {}
@@ -53,11 +55,10 @@ public:
       node_name = std::get<plansys2::Function>(*node_).name;
     } else if (std::holds_alternative<plansys2::Derived>(*node_)) {
       node_name = std::get<plansys2::Derived>(*node_).predicate.name;
-    } else if (std::holds_alternative<plansys2::Action>(*node_)) {
-      node_name = std::get<plansys2::Action>(*node_).name;
-    } else if (std::holds_alternative<plansys2::DurativeAction>(*node_)) {
-      node_name = std::get<plansys2::DurativeAction>(*node_).name;
+    } else if (std::holds_alternative<plansys2::ActionVariant>(*node_)) {
+      node_name =  std::get<plansys2::ActionVariant>(*node_).get_action_name();
     }
+
     return node_name;
   }
 
@@ -147,6 +148,9 @@ public:
     ++edge_count_;
   }
 
+  const auto &getNodeOutEdges(const NodeVariant& node) {return adj_list_[node];}
+  const auto &getNodeInEdges(const NodeVariant& node) {return in_nodes_[node];}
+
   template <typename Func>
   void depthFirstTraverse(
     const NodeVariant& node,
@@ -213,6 +217,44 @@ public:
     }
   }
 
+  template <typename Func>
+  void backtrackTraverse(
+    const NodeVariant& start,
+    Func&& func) const
+  {
+    std::unordered_set<NodeVariant> visited;
+    backtrackTraverse(start, visited, std::forward<Func>(func));
+  }
+
+  template <typename Func>
+  void backtrackTraverse(
+    const NodeVariant& node,
+    std::unordered_set<NodeVariant>& visited,
+    Func&& func) const
+  {
+    // If already visited, skip this node to avoid infinite loops
+    if (visited.find(node) != visited.end()) {
+      return;
+    }
+
+    // Apply the provided function to the current node
+    std::forward<Func>(func)(node);
+    visited.insert(node);
+
+    // Find predecessors (in-nodes)
+    auto it_in_nodes = in_nodes_.find(node);
+    if (it_in_nodes == in_nodes_.end()) {
+      return; // No predecessors to explore
+    }
+
+    // Recursively visit all predecessors
+    for (const auto& predecessor : it_in_nodes->second) {
+      if (visited.find(predecessor) == visited.end()) {
+        backtrackTraverse(predecessor, visited, std::forward<Func>(func));
+      }
+    }
+  }
+
   auto getEdgeNumber() const { return edge_count_; }
 
   void clear() {adj_list_.clear();}
@@ -239,6 +281,28 @@ public:
   : Graph() {}
 
   DerivedGraph(const std::vector<plansys2_msgs::msg::Derived> &derived_predicates)
+  : Graph()
+  {
+    derived_predicates_= convertVectorToUnorderedSet<
+      plansys2::Derived, plansys2_msgs::msg::Derived>(derived_predicates);
+    for (const auto& derived : derived_predicates)
+    {
+      addEdgeFromPreconditions(static_cast<plansys2::Derived>(derived), derived.preconditions);
+    }
+  }
+
+  DerivedGraph(const std::vector<plansys2::Derived> &derived_predicates)
+  : Graph()
+  {
+    derived_predicates_= convertVectorToUnorderedSet<
+      plansys2::Derived, plansys2::Derived>(derived_predicates);
+    for (const auto& derived : derived_predicates)
+    {
+      addEdgeFromPreconditions(derived, derived.preconditions);
+    }
+  }
+
+  DerivedGraph(const std::unordered_set<plansys2::Derived> &derived_predicates)
   : Graph(), derived_predicates_(derived_predicates)
   {
     for (const auto& derived : derived_predicates)
@@ -265,6 +329,39 @@ public:
     return all_nodes;
   }
 
+  std::deque<plansys2::Derived> getDerivedPredicatesFromActions(
+    const std::vector<plansys2::ActionVariant>& actions) const
+  {
+    std::deque<plansys2::Derived> derived_predicates;
+    std::unordered_set<plansys2::NodeVariant> visited;
+    auto func = [&derived_predicates](const plansys2::NodeVariant& node) {
+      if (node.isDerived()) {
+        derived_predicates.push_front(node.getDerivedNode());
+      }
+    };
+    for (const auto& action: actions) {
+      backtrackTraverse(action, visited, func);
+    }
+    return derived_predicates;
+  }
+
+  plansys2::DerivedGraph pruneGraphToActions(const std::vector<plansys2::ActionVariant>& actions)
+  {
+    auto new_graph = DerivedGraph();
+    auto func_new_graph = [this, &new_graph](const plansys2::NodeVariant& node) {
+      for (const auto& parent_node: this->getNodeInEdges(node)) {
+        new_graph.addEdge(parent_node, node);
+      }
+    };
+
+    std::unordered_set<NodeVariant> visited;
+    for (const auto& action: actions)
+    {
+      backtrackTraverse(action, visited, func_new_graph);
+    }
+    return new_graph;
+  }
+
   void addEdgeFromPreconditions(const NodeVariant& node, const plansys2_msgs::msg::Tree& tree)
   {
     for (const auto& tree_node : tree.nodes){
@@ -274,7 +371,7 @@ public:
           bool is_derived_predicate = false;
           for (const auto& d: derived_predicates_) {
             if (parser::pddl::checkNodeEquality(tree_node, d.predicate)){
-              addEdge(d, node);
+              addEdge(static_cast<const plansys2::Derived&>(d), node);
               is_derived_predicate = true;
             }
           }
@@ -293,20 +390,28 @@ public:
     }
   }
 
-  void appendAction(const plansys2::Action& action)
+  void appendAction(const plansys2::ActionVariant& action)
   {
-    addEdgeFromPreconditions(action, action.preconditions);
+    addEdgeFromPreconditions(action, action.get_overall_requirements());
+    if (action.is_durative_action()){
+      addEdgeFromPreconditions(action, action.get_at_start_requirements());
+      addEdgeFromPreconditions(action, action.get_at_end_requirements());
+    }
   }
 
-  void appendDurativeAction(const plansys2::DurativeAction& action)
-  {
-    addEdgeFromPreconditions(action, action.at_start_requirements);
-    addEdgeFromPreconditions(action, action.over_all_requirements);
-    addEdgeFromPreconditions(action, action.at_end_requirements);
+  void addEdge(const NodeVariant& u, const NodeVariant& v){
+    if(u.isDerived())
+    {
+      derived_predicates_.insert(u.getDerivedNode());
+    }
+    if (v.isDerived()) {
+      derived_predicates_.insert(v.getDerivedNode());
+    }
+    Graph::addEdge(u, v);
   }
 
 private:
-  std::vector<plansys2_msgs::msg::Derived> derived_predicates_;
+  std::unordered_set<plansys2::Derived> derived_predicates_;
 
 };
 
