8312c203739f57f5f5521439484885c7889dad94
Gustavo <g.rezendesilva@tudelft.nl>
2024-11-20 20:13:26 +0100

    add tests to Graph and State

--- plansys2_core/test/graph_test.cpp
+++ plansys2_core/test/graph_test.cpp
@@ -228,6 +228,13 @@ TEST(graph_test, graph_derived_constructor)
   ASSERT_EQ(nodes_root_predB.size(), 2);
   ASSERT_EQ(nodes_root_predB[0].predicate.name, "inferredB");
   ASSERT_EQ(nodes_root_predB[1].predicate.name, "inferredBB");
+
+  plansys2::Predicate predA_unified = parser::pddl::fromStringPredicate("(predicateA instance)");
+  auto nodes_root_predA_unified = graph.getDerivedPredicatesDepthFirst({predA_unified});
+  ASSERT_EQ(nodes_root_predA_unified.size(), 3);
+  ASSERT_EQ(nodes_root_predA_unified[0].predicate.name, "inferredAA");
+  ASSERT_EQ(nodes_root_predA_unified[1].predicate.name, "inferredA");
+  ASSERT_EQ(nodes_root_predA_unified[2].predicate.name, "inferredAA");
 }
 
 TEST(graph_test, graph_derived_action)
@@ -235,41 +242,41 @@ TEST(graph_test, graph_derived_action)
   plansys2::Predicate predA = parser::pddl::fromStringPredicate("(predicateA ?a)");
   plansys2::Predicate predB = parser::pddl::fromStringPredicate("(predicateB ?b)");
 
-  std::vector<plansys2_msgs::msg::Derived> derived_predicates;
-  plansys2_msgs::msg::Derived inferredA;
+  std::vector<plansys2::Derived> derived_predicates;
+  plansys2::Derived inferredA;
   inferredA.predicate = parser::pddl::fromStringPredicate("(inferredA ?a)");
   inferredA.predicate.parameters[0].type = "typea";
   inferredA.preconditions = parser::pddl::fromString("(and (predicateA ?a))");
   derived_predicates.push_back(inferredA);
 
-  plansys2_msgs::msg::Derived inferredB;
+  plansys2::Derived inferredB;
   inferredB.predicate = parser::pddl::fromStringPredicate("(inferredB ?b)");
   inferredB.preconditions = parser::pddl::fromString("(and (predicateB ?b))");
   derived_predicates.push_back(inferredB);
 
-  plansys2_msgs::msg::Derived inferredAA;
+  plansys2::Derived inferredAA;
   inferredAA.predicate = parser::pddl::fromStringPredicate("(inferredAA ?aa)");
   inferredAA.preconditions = parser::pddl::fromString("(and (inferredA ?aa))");
   derived_predicates.push_back(inferredAA);
 
-  plansys2_msgs::msg::Derived inferredAA2;
+  plansys2::Derived inferredAA2;
   inferredAA2.predicate = parser::pddl::fromStringPredicate("(inferredAA ?a)");
   inferredAA2.preconditions = parser::pddl::fromString("(and (predicateA ?a))");
   derived_predicates.push_back(inferredAA2);
 
-  plansys2_msgs::msg::Derived inferredBB;
+  plansys2::Derived inferredBB;
   inferredBB.predicate = parser::pddl::fromStringPredicate("(inferredBB ?b)");
   inferredBB.preconditions = parser::pddl::fromString("(and (inferredB ?b))");
   derived_predicates.push_back(inferredBB);
 
-  plansys2_msgs::msg::Derived inferredAB;
+  plansys2::Derived inferredAB;
   inferredAB.predicate = parser::pddl::fromStringPredicate("(inferredAB ?a ?b)");
   inferredAB.preconditions = parser::pddl::fromString("(and (inferredA ?a)(inferredB ?b))");
   derived_predicates.push_back(inferredAB);
 
   plansys2::DerivedGraph graph(derived_predicates);
 
-  plansys2_msgs::msg::Action actionA;
+  plansys2::Action actionA;
   actionA.name = "actionA";
   actionA.parameters.push_back(parser::pddl::fromStringParam("a"));
 
@@ -279,7 +286,7 @@ TEST(graph_test, graph_derived_action)
     "(and (inferredAA a))");
   actionA.preconditions = actionA_preconditions;
 
-  plansys2_msgs::msg::Action actionB;
+  plansys2::Action actionB;
   actionB.name = "actionB";
   actionB.parameters.push_back(parser::pddl::fromStringParam("b"));
 
@@ -289,7 +296,7 @@ TEST(graph_test, graph_derived_action)
     "(and (inferredBB b))");
   actionB.preconditions = actionB_preconditions;
 
-  plansys2_msgs::msg::Action actionAB;
+  plansys2::Action actionAB;
   actionAB.name = "actionAB";
   actionAB.parameters.push_back(parser::pddl::fromStringParam("a"));
   actionAB.parameters.push_back(parser::pddl::fromStringParam("b"));
@@ -325,6 +332,215 @@ TEST(graph_test, graph_derived_action)
   ASSERT_EQ(all_nodes[8], "actionAB");
   ASSERT_EQ(all_nodes[9], "inferredAA");
   ASSERT_EQ(all_nodes[10], "actionA");
+
+  std::vector<std::string> actionAB_parent_nodes;
+  auto func_actionAB_parents = [&actionAB_parent_nodes](const plansys2::NodeVariant& node) {
+    actionAB_parent_nodes.push_back(node.getNodeName());
+  };
+  graph.backtrackTraverse(
+    actionAB,
+    func_actionAB_parents
+  );
+
+  ASSERT_EQ(actionAB_parent_nodes.size(), 6);
+  ASSERT_EQ(actionAB_parent_nodes[0], "actionAB");
+  ASSERT_EQ(actionAB_parent_nodes[1], "inferredAB");
+  ASSERT_EQ(actionAB_parent_nodes[2], "inferredB");
+  ASSERT_EQ(actionAB_parent_nodes[3], "predicateB");
+  ASSERT_EQ(actionAB_parent_nodes[4], "inferredA");
+  ASSERT_EQ(actionAB_parent_nodes[5], "predicateA");
+
+  std::vector<std::string> actionA_parent_nodes;
+  auto func_actionA_parents = [&actionA_parent_nodes](const plansys2::NodeVariant& node) {
+    actionA_parent_nodes.push_back(node.getNodeName());
+  };
+  graph.backtrackTraverse(
+    actionA,
+    func_actionA_parents
+  );
+
+  ASSERT_EQ(actionA_parent_nodes.size(), 5);
+  ASSERT_EQ(actionA_parent_nodes[0], "actionA");
+  ASSERT_EQ(actionA_parent_nodes[1], "inferredAA");
+  ASSERT_EQ(actionA_parent_nodes[2], "inferredA");
+  ASSERT_EQ(actionA_parent_nodes[3], "predicateA");
+  ASSERT_EQ(actionA_parent_nodes[4], "inferredAA");
+
+  std::vector<std::string> actionB_parent_nodes;
+  auto func_actionB_parents = [&actionB_parent_nodes](const plansys2::NodeVariant& node) {
+    actionB_parent_nodes.push_back(node.getNodeName());
+  };
+  graph.backtrackTraverse(
+    actionB,
+    func_actionB_parents
+  );
+
+  ASSERT_EQ(actionB_parent_nodes.size(), 4);
+  ASSERT_EQ(actionB_parent_nodes[0], "actionB");
+  ASSERT_EQ(actionB_parent_nodes[1], "inferredBB");
+  ASSERT_EQ(actionB_parent_nodes[2], "inferredB");
+  ASSERT_EQ(actionB_parent_nodes[3], "predicateB");
+
+  auto actionAB_parent_nodes_2 = graph.getDerivedPredicatesFromActions({actionAB});
+  ASSERT_EQ(actionAB_parent_nodes_2.size(), 3);
+  ASSERT_EQ(actionAB_parent_nodes_2[0].predicate.name, "inferredA");
+  ASSERT_EQ(actionAB_parent_nodes_2[1].predicate.name, "inferredB");
+  ASSERT_EQ(actionAB_parent_nodes_2[2].predicate.name, "inferredAB");
+
+  auto graph_actionA = graph.pruneGraphToActions({actionA});
+
+  all_nodes.clear();
+  graph_actionA.depthFirstTraverseAll(
+    func_all,
+    true
+  );
+
+  ASSERT_EQ(all_nodes.size(), 5);
+  ASSERT_EQ(all_nodes[0], "predicateA");
+  ASSERT_EQ(all_nodes[1], "inferredAA");
+  ASSERT_EQ(all_nodes[2], "inferredA");
+  ASSERT_EQ(all_nodes[3], "inferredAA");
+  ASSERT_EQ(all_nodes[4], "actionA");
+
+  actionA_parent_nodes.clear();
+  graph_actionA.backtrackTraverse(
+    actionA,
+    func_actionA_parents
+  );
+
+  ASSERT_EQ(actionA_parent_nodes.size(), 5);
+  ASSERT_EQ(actionA_parent_nodes[0], "actionA");
+  ASSERT_EQ(actionA_parent_nodes[1], "inferredAA");
+  ASSERT_EQ(actionA_parent_nodes[2], "predicateA");
+  ASSERT_EQ(actionA_parent_nodes[3], "inferredAA");
+  ASSERT_EQ(actionA_parent_nodes[4], "inferredA");
+
+  auto derived_predicates_A = graph_actionA.getDerivedPredicatesDepthFirst();
+  ASSERT_EQ(derived_predicates_A.size(), 3);
+  ASSERT_EQ(derived_predicates_A[0].predicate.name, "inferredAA");
+  ASSERT_EQ(derived_predicates_A[1].predicate.name, "inferredA");
+  ASSERT_EQ(derived_predicates_A[2].predicate.name, "inferredAA");
+
+  auto graph_actionB = graph.pruneGraphToActions({actionB});
+
+  all_nodes.clear();
+  graph_actionB.depthFirstTraverseAll(
+    func_all,
+    true
+  );
+
+  ASSERT_EQ(all_nodes.size(), 4);
+  ASSERT_EQ(all_nodes[0], "predicateB");
+  ASSERT_EQ(all_nodes[1], "inferredB");
+  ASSERT_EQ(all_nodes[2], "inferredBB");
+  ASSERT_EQ(all_nodes[3], "actionB");
+
+  actionB_parent_nodes.clear();
+  graph_actionB.backtrackTraverse(
+    actionB,
+    func_actionB_parents
+  );
+
+  ASSERT_EQ(actionB_parent_nodes.size(), 4);
+  ASSERT_EQ(actionB_parent_nodes[0], "actionB");
+  ASSERT_EQ(actionB_parent_nodes[1], "inferredBB");
+  ASSERT_EQ(actionB_parent_nodes[2], "inferredB");
+  ASSERT_EQ(actionB_parent_nodes[3], "predicateB");
+
+  auto derived_predicates_B = graph_actionB.getDerivedPredicatesDepthFirst();
+  ASSERT_EQ(derived_predicates_B.size(), 2);
+  ASSERT_EQ(derived_predicates_B[0].predicate.name, "inferredB");
+  ASSERT_EQ(derived_predicates_B[1].predicate.name, "inferredBB");
+
+  auto graph_actionAB = graph.pruneGraphToActions({actionAB});
+
+  all_nodes.clear();
+  graph_actionAB.depthFirstTraverseAll(
+    func_all,
+    true
+  );
+
+  ASSERT_EQ(all_nodes.size(), 6);
+  ASSERT_EQ(all_nodes[0], "predicateA");
+  ASSERT_EQ(all_nodes[1], "inferredA");
+  ASSERT_EQ(all_nodes[2], "predicateB");
+  ASSERT_EQ(all_nodes[3], "inferredB");
+  ASSERT_EQ(all_nodes[4], "inferredAB");
+  ASSERT_EQ(all_nodes[5], "actionAB");
+
+  actionAB_parent_nodes.clear();
+  graph_actionAB.backtrackTraverse(
+    actionAB,
+    func_actionAB_parents
+  );
+
+  ASSERT_EQ(actionAB_parent_nodes.size(), 6);
+  ASSERT_EQ(actionAB_parent_nodes[0], "actionAB");
+  ASSERT_EQ(actionAB_parent_nodes[1], "inferredAB");
+  ASSERT_EQ(actionAB_parent_nodes[2], "inferredA");
+  ASSERT_EQ(actionAB_parent_nodes[3], "predicateA");
+  ASSERT_EQ(actionAB_parent_nodes[4], "inferredB");
+  ASSERT_EQ(actionAB_parent_nodes[5], "predicateB");
+
+  auto derived_predicatesAB = graph_actionAB.getDerivedPredicatesDepthFirst();
+  ASSERT_EQ(derived_predicatesAB.size(), 3);
+  ASSERT_EQ(derived_predicatesAB[0].predicate.name, "inferredA");
+  ASSERT_EQ(derived_predicatesAB[1].predicate.name, "inferredB");
+  ASSERT_EQ(derived_predicatesAB[2].predicate.name, "inferredAB");
+
+  auto graph_actionA_AB = graph.pruneGraphToActions({actionA, actionAB});
+
+  all_nodes.clear();
+  graph_actionA_AB.depthFirstTraverseAll(
+    func_all,
+    true
+  );
+
+  ASSERT_EQ(all_nodes.size(), 9);
+  ASSERT_EQ(all_nodes[0], "predicateB");
+  ASSERT_EQ(all_nodes[1], "inferredB");
+  ASSERT_EQ(all_nodes[2], "predicateA");
+  ASSERT_EQ(all_nodes[3], "inferredAA");
+  ASSERT_EQ(all_nodes[4], "inferredA");
+  ASSERT_EQ(all_nodes[5], "inferredAB");
+  ASSERT_EQ(all_nodes[6], "actionAB");
+  ASSERT_EQ(all_nodes[7], "inferredAA");
+  ASSERT_EQ(all_nodes[8], "actionA");
+
+  actionA_parent_nodes.clear();
+  graph_actionA_AB.backtrackTraverse(
+    actionA,
+    func_actionA_parents
+  );
+
+  ASSERT_EQ(actionA_parent_nodes.size(), 5);
+  ASSERT_EQ(actionA_parent_nodes[0], "actionA");
+  ASSERT_EQ(actionA_parent_nodes[1], "inferredAA");
+  ASSERT_EQ(actionA_parent_nodes[2], "predicateA");
+  ASSERT_EQ(actionA_parent_nodes[3], "inferredAA");
+  ASSERT_EQ(actionA_parent_nodes[4], "inferredA");
+
+  actionAB_parent_nodes.clear();
+  graph_actionA_AB.backtrackTraverse(
+    actionAB,
+    func_actionAB_parents
+  );
+
+  ASSERT_EQ(actionAB_parent_nodes.size(), 6);
+  ASSERT_EQ(actionAB_parent_nodes[0], "actionAB");
+  ASSERT_EQ(actionAB_parent_nodes[1], "inferredAB");
+  ASSERT_EQ(actionAB_parent_nodes[2], "inferredA");
+  ASSERT_EQ(actionAB_parent_nodes[3], "predicateA");
+  ASSERT_EQ(actionAB_parent_nodes[4], "inferredB");
+  ASSERT_EQ(actionAB_parent_nodes[5], "predicateB");
+
+  auto derived_predicates_A_AB = graph_actionA_AB.getDerivedPredicatesDepthFirst();
+  ASSERT_EQ(derived_predicates_A_AB.size(), 5);
+  ASSERT_EQ(derived_predicates_A_AB[0].predicate.name, "inferredB");
+  ASSERT_EQ(derived_predicates_A_AB[1].predicate.name, "inferredAA");
+  ASSERT_EQ(derived_predicates_A_AB[2].predicate.name, "inferredA");
+  ASSERT_EQ(derived_predicates_A_AB[3].predicate.name, "inferredAB");
+  ASSERT_EQ(derived_predicates_A_AB[4].predicate.name, "inferredAA");
 }
 
 int main(int argc, char ** argv)
---
+++ plansys2_core/test/state_test.cpp
@@ -0,0 +1,172 @@
+// Copyright 2024 Intelligent Robotics Lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gtest/gtest.h"
+
+#include "plansys2_core/Graph.hpp"
+#include "plansys2_core/State.hpp"
+
+TEST(state_test, state)
+{
+  plansys2::Predicate predA = parser::pddl::fromStringPredicate("(predicateA ?a)");
+  plansys2::Predicate predB = parser::pddl::fromStringPredicate("(predicateB ?b)");
+
+  std::vector<plansys2_msgs::msg::Derived> derived_predicates;
+  plansys2_msgs::msg::Derived inferredA;
+  inferredA.predicate = parser::pddl::fromStringPredicate("(inferredA ?a)");
+  inferredA.predicate.parameters[0].type = "typea";
+  inferredA.preconditions = parser::pddl::fromString("(and (predicateA ?a))");
+  derived_predicates.push_back(inferredA);
+
+  plansys2_msgs::msg::Derived inferredB;
+  inferredB.predicate = parser::pddl::fromStringPredicate("(inferredB ?b)");
+  inferredB.preconditions = parser::pddl::fromString("(and (predicateB ?b))");
+  derived_predicates.push_back(inferredB);
+
+  plansys2_msgs::msg::Derived inferredAA;
+  inferredAA.predicate = parser::pddl::fromStringPredicate("(inferredAA ?aa)");
+  inferredAA.preconditions = parser::pddl::fromString("(and (inferredA ?aa))");
+  derived_predicates.push_back(inferredAA);
+
+  plansys2_msgs::msg::Derived inferredAA2;
+  inferredAA2.predicate = parser::pddl::fromStringPredicate("(inferredAA ?a)");
+  inferredAA2.preconditions = parser::pddl::fromString("(and (predicateA ?a))");
+  derived_predicates.push_back(inferredAA2);
+
+  plansys2_msgs::msg::Derived inferredBB;
+  inferredBB.predicate = parser::pddl::fromStringPredicate("(inferredBB ?b)");
+  inferredBB.preconditions = parser::pddl::fromString("(and (inferredB ?b))");
+  derived_predicates.push_back(inferredBB);
+
+  plansys2_msgs::msg::Derived inferredAB;
+  inferredAB.predicate = parser::pddl::fromStringPredicate("(inferredAB ?p)");
+  inferredAB.preconditions = parser::pddl::fromString("(and (inferredA ?p)(inferredB ?p))");
+  derived_predicates.push_back(inferredAB);
+
+  plansys2::DerivedGraph graph(derived_predicates);
+
+  plansys2::Instance instanceA = parser::pddl::fromStringParam("(instanceA)");
+  plansys2::Instance instanceB = parser::pddl::fromStringParam("(instanceB)");
+  plansys2::Instance instanceAB = parser::pddl::fromStringParam("(instanceAB)");
+  std::unordered_set<plansys2::Instance> instances;
+  instances.insert(instanceA);
+  instances.insert(instanceB);
+  instances.insert(instanceAB);
+
+  plansys2::Predicate predA_instanceA = parser::pddl::fromStringPredicate("(predicateA instanceA)");
+  plansys2::Predicate predA_instanceAB = parser::pddl::fromStringPredicate("(predicateA instanceAB)");
+  plansys2::Predicate predB_instanceB = parser::pddl::fromStringPredicate("(predicateB instanceB)");
+  plansys2::Predicate predB_instanceAB = parser::pddl::fromStringPredicate("(predicateB instanceAB)");
+  std::unordered_set<plansys2::Predicate> predicates;
+  predicates.insert(predA_instanceA);
+  predicates.insert(predA_instanceAB);
+  predicates.insert(predB_instanceB);
+  predicates.insert(predB_instanceAB);
+
+  plansys2::Predicate inferredA_instanceA = parser::pddl::fromStringPredicate("(inferredA instanceA)");
+  plansys2::Predicate inferredAA_instanceA = parser::pddl::fromStringPredicate("(inferredAA instanceA)");
+  plansys2::Predicate inferredAA2_instanceA = parser::pddl::fromStringPredicate("(inferredAA instanceA)");
+
+  plansys2::Predicate inferredA_instanceAB = parser::pddl::fromStringPredicate("(inferredA instanceAB)");
+  plansys2::Predicate inferredAA_instanceAB = parser::pddl::fromStringPredicate("(inferredAA instanceAB)");
+  plansys2::Predicate inferredAA2_instanceAB = parser::pddl::fromStringPredicate("(inferredAA instanceAB)");
+
+  plansys2::Predicate inferredB_instanceB = parser::pddl::fromStringPredicate("(inferredB instanceB)");
+  plansys2::Predicate inferredBB_instanceB = parser::pddl::fromStringPredicate("(inferredBB instanceB)");
+
+  plansys2::Predicate inferredB_instanceAB = parser::pddl::fromStringPredicate("(inferredB instanceAB)");
+  plansys2::Predicate inferredBB_instanceAB = parser::pddl::fromStringPredicate("(inferredBB instanceAB)");
+
+  plansys2::Predicate inferredAB_instanceAB = parser::pddl::fromStringPredicate("(inferredAB instanceAB)");
+
+  std::unordered_set<plansys2::Function> functions;
+  plansys2::State state(instances, functions, predicates);
+
+  ASSERT_EQ(state.getInstancesSize(), 3);
+  ASSERT_EQ(state.getPredicatesSize(), 4);
+  ASSERT_EQ(state.getFunctionsSize(), 0);
+  ASSERT_EQ(state.getInferredPredicatesSize(), 4);
+
+  state.addInferredPredicate(inferredA_instanceA);
+  state.addInferredPredicate(inferredAA_instanceA);
+  state.addInferredPredicate(inferredAA2_instanceA);
+
+  state.addInferredPredicate(inferredA_instanceAB);
+  state.addInferredPredicate(inferredAA_instanceAB);
+  state.addInferredPredicate(inferredAA2_instanceAB);
+
+  state.addInferredPredicate(inferredB_instanceB);
+  state.addInferredPredicate(inferredBB_instanceB);
+
+  state.addInferredPredicate(inferredB_instanceAB);
+  state.addInferredPredicate(inferredBB_instanceAB);
+
+  state.addInferredPredicate(inferredAB_instanceAB);
+
+  ASSERT_EQ(state.getInferredPredicatesSize(), 13);
+  ASSERT_EQ(state.getNumberInferredFromDerived(inferredA), 2);
+  ASSERT_EQ(state.getNumberInferredFromDerived(inferredB), 2);
+  ASSERT_EQ(state.getNumberInferredFromDerived(inferredAA), 2);
+  ASSERT_EQ(state.getNumberInferredFromDerived(inferredBB), 2);
+  ASSERT_EQ(state.getNumberInferredFromDerived(inferredAB), 1);
+
+  std::unordered_set<plansys2::Predicate> inferred_predicates;
+  inferred_predicates.insert(inferredA_instanceA);
+  inferred_predicates.insert(inferredAA_instanceA);
+  inferred_predicates.insert(inferredAA2_instanceA);
+
+  inferred_predicates.insert(inferredA_instanceAB);
+  inferred_predicates.insert(inferredAA_instanceAB);
+  inferred_predicates.insert(inferredAA2_instanceAB);
+
+  inferred_predicates.insert(inferredB_instanceB);
+  inferred_predicates.insert(inferredBB_instanceB);
+
+  inferred_predicates.insert(inferredB_instanceAB);
+  inferred_predicates.insert(inferredBB_instanceAB);
+
+  inferred_predicates.insert(inferredAB_instanceAB);
+
+  plansys2::State state_2(instances, functions, predicates, inferred_predicates, graph);
+
+  ASSERT_EQ(state_2.getInferredPredicatesSize(), 13);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredA), 2);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredB), 2);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredAA), 2);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredBB), 2);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredAB), 1);
+
+  state_2.ungroundDerivedPredicate(inferredA);
+  ASSERT_EQ(state_2.getInferredPredicatesSize(), 8);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredA), 0);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredAA), 0);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredAB), 0);
+
+  state_2.ungroundDerivedPredicate(inferredA);
+  ASSERT_EQ(state_2.getInferredPredicatesSize(), 8);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredA), 0);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredAA), 0);
+  ASSERT_EQ(state_2.getNumberInferredFromDerived(inferredAB), 0);
+
+  plansys2::State state_3(instances, functions, predicates, inferred_predicates, graph);
+  state_3.ungroundDerivedPredicate(inferredAB);
+  ASSERT_EQ(state_3.getInferredPredicatesSize(), 12);
+  ASSERT_EQ(state_3.getNumberInferredFromDerived(inferredAB), 0);
+}
+
+int main(int argc, char ** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
